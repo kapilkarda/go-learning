bed093ee997eb28c34d82aabce43774940d11e65
kapilkarda <kkarda77@gmail.com>
2023-05-19 16:06:26 +0530

    Code

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/kapilkarda/go-learning/handlers"
+)
+
+func main() {
+
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+
+	http.ListenAndServe(":9090", sm)
+}
